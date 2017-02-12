e864b9153cd239d4344299e2c96b2976a1969592
Stephan Ziesenis <ziesenis@stud.hs-heilbronn.de>
2017-02-12 12:43:30 +0100

    Renamed Var

--- backend/src/userFunctions.php
+++ backend/src/userFunctions.php
@@ -1,15 +1,15 @@
 <?php	
 	// Vars defining the JSON Keys
 	$httpTableUsers = 'users';
-	$usersJSON = 'Users';
-	$userJSON = 'User';
+	$JSON_Users = 'Users';
+	$JSON_User = 'User';
 	
 	// Convert SQL Result-User to User Object; int and boolean casting
 	function outputUsersResult($httpKey, $sqlResult)
 	{	
-		global $usersJSON;
-		global $userJSON;		
-		if (!$httpKey) echo "{\"$usersJSON\":[";
+		global $JSON_Users;
+		global $JSON_User;		
+		if (!$httpKey) echo "{\"$JSON_Users\":[";
 		$rowCount = mysqli_num_rows($sqlResult);
 		for ($i=0;$i<$rowCount;$i++) 
 		{
@@ -17,14 +17,14 @@
 			$user->ID = (int)$user->ID;
 			$user->u_active = ($user->u_active == 1?true:false);
 			if($i > 0) echo ",";
-			echo "{"."\"$userJSON\":".json_encode($user)."}";
+			echo "{"."\"$JSON_User\":".json_encode($user)."}";
 		}			
 		if (!$httpKey) echo "]}";
 	}
 	
 	function getUserSQLSetStringFromHttpInput($httpInput)
 	{	
-		global $userJSON; // Using Global to get the var into the function
+		global $JSON_User; // Using Global to get the var into the function
 		$set = '';
 		$userSurname = 'u_surname';
 		$userForename = 'u_forename';
@@ -33,12 +33,12 @@
 		$userUsername = 'u_username';
 		$userPassword = 'u_password';
 		$userActive = 'u_active';
-		$set.= "$userSurname = \"".$httpInput[$userJSON][$userSurname]."\", ";
-		$set.= "$userForename = \"".$httpInput[$userJSON][$userForename]."\", ";
-		$set.= "$userNumber = \"".$httpInput[$userJSON][$userNumber]."\", ";
-		$set.= "$userMail = \"".$httpInput[$userJSON][$userMail]."\", ";
-		$set.= "$userUsername = \"".$httpInput[$userJSON][$userUsername]."\", ";
-		$set.= "$userPassword = \"".$httpInput[$userJSON][$userPassword]."\", ";
-		$set.= "$userActive = ".($httpInput[$userJSON][$userActive]==1?"true":"false");
+		$set.= "$userSurname = \"".$httpInput[$JSON_User][$userSurname]."\", ";
+		$set.= "$userForename = \"".$httpInput[$JSON_User][$userForename]."\", ";
+		$set.= "$userNumber = \"".$httpInput[$JSON_User][$userNumber]."\", ";
+		$set.= "$userMail = \"".$httpInput[$JSON_User][$userMail]."\", ";
+		$set.= "$userUsername = \"".$httpInput[$JSON_User][$userUsername]."\", ";
+		$set.= "$userPassword = \"".$httpInput[$JSON_User][$userPassword]."\", ";
+		$set.= "$userActive = ".($httpInput[$JSON_User][$userActive]==1?"true":"false");
 		return $set;
 	}
